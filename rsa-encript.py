884e3c9bda4b94cccb95cc210b3dc8851eaabbf5
Victor De la Luz <itztli@gmail.com>
2026-05-11 10:10:47 -0600

    updating example in paper

---
+++ rsa-encript.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import math 
+
+p = 47
+q = 59
+
+n = p*q
+
+d = 157
+e = 17
+
+e = int(input("Public Key: "))
+n = int(input("Module n: "))
+M = input("Enter some text: ")
+
+#M = "ITS ALL GREEK TO ME"
+
+print(M)
+encode = {' ': '00'}
+
+encode.update({chr(65 + i): str(i + 1).zfill(2) for i in range(26)})
+
+N = len(M)
+
+Menc = []
+for i in range(int(N/2)):    
+    c = encode[M[2*i]] + encode[M[2*i+1]]
+    #print(c)
+    Menc.append(c)
+if (N % 2 == 1):
+    c = encode[M[N-1]] + "00"
+    Menc.append(c)
+    #print(c)
+
+#print(Menc)
+C=[]
+for i in range(len(Menc)):
+    m = int(Menc[i])
+    c = pow(m,e,n)
+    #print(str(m)+"^"+str(e)+"%"+str(n)+"="+str(c))
+    #c = multi % n 
+    C.append(str(c).zfill(4))
+    #print(encode[M[i]], pow(encode[M[i]],e))
+    
+print(*C)
+
---
+++ rsa-txt.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import math 
+
+p = 47
+q = 59
+
+n = p*q
+
+d = 157
+e = 17
+
+M = input("Enter some text: ")
+
+#M = "ITS ALL GREEK TO ME"
+
+print(M)
+encode = {' ': '00'}
+
+encode.update({chr(65 + i): str(i + 1).zfill(2) for i in range(26)})
+
+N = len(M)
+
+Menc = []
+for i in range(int(N/2)):    
+    c = encode[M[2*i]] + encode[M[2*i+1]]
+    #print(c)
+    Menc.append(c)
+if (N % 2 == 1):
+    c = encode[M[N-1]] + "00"
+    Menc.append(c)
+    #print(c)
+
+#print(Menc)
+C=[]
+for i in range(len(Menc)):
+    m = int(Menc[i])
+    c = pow(m,e,n)
+    #print(str(m)+"^"+str(e)+"%"+str(n)+"="+str(c))
+    #c = multi % n 
+    C.append(str(c).zfill(4))
+    #print(encode[M[i]], pow(encode[M[i]],e))
+    
+print(C)
+
+decode = {v: k for k, v in encode.items()}
+#for c in C:
+#    M1 = c[0:2]
+#    M2 = c[2:4]
+#    print(decode[M1]+decode[M2])
+
+for i in range(len(C)):
+    Mdec = str(pow(int(C[i]), d, n)).zfill(4)
+    M1 = Mdec[0:2]
+    M2 = Mdec[2:4]
+    print(decode[M1]+decode[M2],end="")
+print("")
